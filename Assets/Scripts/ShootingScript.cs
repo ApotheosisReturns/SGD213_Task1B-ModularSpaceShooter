b126d80c7a11ed3d9dc309e4f20bea48fbf9e63b
Ronaldo Harvey <rdh017@student.usc.edu.au>
2026-04-01 12:04:09 +1000

    Update Shooting Script
    Removed unused code
    -System.Collections and SampleMethod()
    Cached components
    
    Split logic into methods
    CanFire() → handles cooldown
    Fire() → handles shooting
    
    Makes it easier to expand later (sound, effects)
    
    Cleaner math
    
    Safer timing check

--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -1,55 +1,50 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class ShootingScript : MonoBehaviour
 {
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float fireDelay = 1f;
 
-    [SerializeField]
-    private GameObject bullet;
+    private float lastFiredTime;
+    private float bulletOffset;
 
-    private float lastFiredTime = 0f;
+    private Renderer playerRenderer;
+    private Renderer bulletRenderer;
 
-    [SerializeField]
-    private float fireDelay = 1f;
-
-    private float bulletOffset = 2f;
-
-    void Start()
+    private void Awake()
     {
-        // Do some math to perfectly spawn bullets in front of us
-        bulletOffset = GetComponent<Renderer>().bounds.size.y / 2 // Half of our size
-            + bullet.GetComponent<Renderer>().bounds.size.y / 2; // Plus half of the bullet size
+        playerRenderer = GetComponent<Renderer>();
+        bulletRenderer = bullet.GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        if (Input.GetButton("Fire1"))
+        // Calculate spawn offset once
+        bulletOffset = (playerRenderer.bounds.size.y * 0.5f) +
+                       (bulletRenderer.bounds.size.y * 0.5f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && CanFire())
         {
-            float CurrentTime = Time.time;
-
-            // Have a delay so we don't shoot too many bullets
-            if (CurrentTime - lastFiredTime > fireDelay)
-            {
-                Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y + bulletOffset);
-
-                Instantiate(bullet, spawnPosition, transform.rotation);
-
-                lastFiredTime = CurrentTime;
-            }
-
-            //print("Shoot!");
+            Fire();
         }
     }
 
-    /// <summary>
-    /// SampleMethod is a sample of how to use abstraction by
-    /// specification. It converts a provided integer to a float.
-    /// </summary>
-    /// <param name="number">any integer</param>
-    /// <returns>the number parameter as a float</returns>
-    public float SampleMethod(int number) {
-        return number;
+    private bool CanFire()
+    {
+        return Time.time - lastFiredTime >= fireDelay;
     }
 
+    private void Fire()
+    {
+        Vector2 spawnPosition = new Vector2(
+            transform.position.x,
+            transform.position.y + bulletOffset
+        );
+
+        Instantiate(bullet, spawnPosition, transform.rotation);
+        lastFiredTime = Time.time;
+    }
 }
